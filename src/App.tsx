f94cdf5b7efd6abb1429f9527604b20f2520e630
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2023-08-31 13:07:18 +0530

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { Layout } from './components/Layout'
 
 function App() {
 
   return (
     <>
-     
+     <Layout/>
     </>
   )
 }
---
+++ src/components/Layout.tsx
@@ -0,0 +1,9 @@
+export const Layout = () => {
+  return (
+    <>
+      <div className="text-white w-4 bg-slate-400 p-3 font-bold m-4  text-center">
+        Hello
+      </div>
+    </>
+  );
+};
