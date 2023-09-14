769a87220ca96d406ced691e18462c2402af9206
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2023-09-14 18:24:26 +0530

    tv series type added

--- src/api/fetchTrending.ts
+++ src/api/fetchTrending.ts
@@ -4,14 +4,11 @@ import { movieType } from "./fetchTypes.ts";
 export default function fetchTrendingMovies () {
     const url =
     "https://api.themoviedb.org/3/trending/movie/day?language=en-US";
-  const options = {
-    //method: "GET",
+    const options = {
     headers: {
       accept: "application/json",
       Authorization: import.meta.env.VITE_AUTHORIZATION,
     },
   };
-//    return fetch(url, options)
-//     .then((res) => res.json())
     return axios.get<movieType>(url, options).then((res) => res.data);
 }
