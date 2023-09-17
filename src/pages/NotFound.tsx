a15b5d7cfa55c18514221d2496182314d3519151
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2023-09-17 18:08:46 +0530

    search api

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const NotFound = () => {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <div className=" text-red-600 text-5xl font-extrabold text-center">
+        YOUR SEARCH NOT FOUND
+      </div>
+    </div>
+  );
+};
