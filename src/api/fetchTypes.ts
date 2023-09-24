459ec14d7c239e437753cc30275309e72c14446e
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2023-09-24 17:56:14 +0530

    design detail page

--- src/api/fetchTypes.ts
+++ src/api/fetchTypes.ts
@@ -1,121 +1,120 @@
 export type movieType = {
-    page: number;
-    results: [
-      {
-        adult: boolean;
-        backdrop_path: string;
-        id: number;
-        title: string;
-        original_language: string;
-        original_title: string;
-        overview: string;
-        poster_path: string;
-        media_type: string;
-        genre_ids: number[];
-        popularity: number;
-        release_date: string;
-        video: boolean;
-        vote_average: number;
-        vote_count: number;
-      }
-    ];
+  page: number;
+  results: [
+    {
+      adult: boolean;
+      backdrop_path: string;
+      id: number;
+      title: string;
+      original_language: string;
+      original_title: string;
+      overview: string;
+      poster_path: string;
+      media_type: string;
+      genre_ids: number[];
+      popularity: number;
+      release_date: string;
+      video: boolean;
+      vote_average: number;
+      vote_count: number;
+    }
+  ];
 };
 
 export type seriesType = {
-    page: number,
-    results: [
-      {
-        adult: boolean,
-        backdrop_path: string,
-        id: number,
-        name: string,
-        original_language: string,
-        original_name: string,
-        overview: string,
-        poster_path: string,
-        media_type: string,
-        genre_ids: number[],
-        popularity: number,
-        first_air_date: string,
-        vote_average: number,
-        vote_count: number,
-        origin_country: string[]
-      }
-    ];
-  };
+  page: number;
+  results: [
+    {
+      adult: boolean;
+      backdrop_path: string;
+      id: number;
+      name: string;
+      original_language: string;
+      original_name: string;
+      overview: string;
+      poster_path: string;
+      media_type: string;
+      genre_ids: number[];
+      popularity: number;
+      first_air_date: string;
+      vote_average: number;
+      vote_count: number;
+      origin_country: string[];
+    }
+  ];
+};
 
-  export type searchResult = {
-    adult: boolean;
-    backdrop_path: string;
+export type searchResult = {
+  adult: boolean;
+  backdrop_path: string;
+  id: number;
+  name?: string;
+  title?: string;
+  original_language: string;
+  original_name: string;
+  overview: string;
+  poster_path: string;
+  media_type: string;
+  genre_ids: number[];
+  popularity: number;
+  release_date?: string;
+  first_air_date?: string;
+  vote_average: number;
+  vote_count: number;
+  origin_country: string[];
+};
+
+export type searchType = {
+  page?: number;
+  results: searchResult[];
+};
+
+export type details = {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: {
     id: number;
-    name?: string;
-    title?: string;
-    original_language: string;
-    original_name: string;
-    overview: string;
+    name: string;
     poster_path: string;
-    media_type: string;
-    genre_ids: number[];
-    popularity: number;
-    release_date?: string;
-    first_air_date?: string;
-    vote_average: number;
-    vote_count: number;
-    origin_country: string[];
-    
-  }
-  
-  export type searchType = {
-    page?: number;
-    results: searchResult[];
-  }
-  
-  export type details = {
-    adult: boolean;
     backdrop_path: string;
-    belongs_to_collection: {
-      id: number,
-      name: string,
-      poster_path: string,
-      backdrop_path: string
-    },
-    budget: number,
-    genres: [
-      {
-        id: number,
-        name: string
-      },
-    ],
-    homepage: string, // url for movie homepage
-    id: number,
-    imdb_id: string, //https://www.imdb.com/title/tt10638522/?ref_=hm_top_tt_i_3
-    original_language:string,
-    original_title: string,
-    overview: string,
-    popularity: number,
-    poster_path: string,
-    release_date: string,
-    revenue: number,
-    runtime: number,
-    status: string,
-    tagline: string,
-    title:string,
-    video: boolean,
+  };
+  budget: number;
+  genres: [
+    {
+      id: number;
+      name: string;
+    }
+  ];
+  homepage: string; // url for movie homepage
+  id: number;
+  imdb_id: string; //https://www.imdb.com/title/tt10638522/?ref_=hm_top_tt_i_3
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
 
-    production_companies: [
-      {
-        id: number,
-        name: string,
-        logo_path: string,
-        origin_country: string
-      }],
-    production_countries: [
-        {
-          iso_3166_1: string,
-          name: string,
-        }
-      ],
-      vote_average: number,
-      vote_count: number
-
-  }
+  production_companies: [
+    {
+      id: number;
+      name: string;
+      logo_path: string;
+      origin_country: string;
+    }
+  ];
+  production_countries: [
+    {
+      iso_3166_1: string;
+      name: string;
+    }
+  ];
+  vote_average: number;
+  vote_count: number;
+}
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -3,6 +3,8 @@ import { fetchDetails } from "../api/fetchDetails";
 import { useState } from "react";
 import { details } from "../api/fetchTypes";
 
+import not_found_image from "../assets/not_found.jpg";
+
 export const Details = () => {
   const { mediaType, id } = useParams();
 
@@ -16,14 +18,38 @@ export const Details = () => {
       })
       .catch((error) => console.log(error));
   };
+  mediaType == "movie" && getBaseDetails("movie");
+  mediaType == "tv" && getBaseDetails("tv");
   return (
-    <>
-      {" "}
-      {/*card media type is {mediaType} and id is {id}*/}
-      {mediaType == "movie" && getBaseDetails("movie")}
-      {mediaType == "tv" && getBaseDetails("tv")}
-      {/* create detail page below */}
-      {detail?.overview}
-    </>
+    <div className="h-screen bg-gray-500">
+      <div className="pt-20 pl-44 flex flex-row">
+        <img
+          src={`${
+            detail?.poster_path != null
+              ? `https://image.tmdb.org/t/p/original${detail.poster_path}`
+              : `${not_found_image}`
+          }`}
+          alt=""
+          className={"w-72 border-white border-8"}
+        />
+        <div className="basis-[50%] pl-9 pt-9 text-left text-white">
+          <p className="text-2xl font-bold mb-5">{detail?.title}</p>
+          <p className="text-xl font-bold ">
+            Release Date : {detail?.release_date}
+          </p>
+          <div className="flex flex-row text-xl font-bold">
+            {detail?.genres.map((value, id) =>
+              id === 0 ? (
+                <p key={id}>{value.name}</p>
+              ) : (
+                <p key={id}>/{value.name}</p>
+              )
+            )}
+          </div>
+        </div>
+
+        <p className="text-2xl font-bold ">{detail?.title}</p>
+      </div>
+    </div>
   );
 };
