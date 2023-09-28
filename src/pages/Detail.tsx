17ff79f93a1a7bd36e0162263a74b0251e3c9348
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2023-09-28 17:59:14 +0530

    Responsive Detail Page

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -52,8 +52,8 @@ export const Details = () => {
   mediaType == "tv" && getDetails("tv");
 
   return (
-    <div className="h-full bg-gray-500">
-      <section className="pt-20 lg:pl-36 pl-24 flex flex-row">
+    <div className="w-full h-full bg-gray-500">
+      <section className="flex md:flex-row flex-col pt-20 lg:pl-36 pl-10 ">
         <img
           src={`${
             detail?.poster_path != null
@@ -61,9 +61,9 @@ export const Details = () => {
               : `${not_found_image}`
           }`}
           alt=""
-          className={"w-72 border-white border-8"}
+          className={"lg:w-72 w-32 border-white border-8"}
         />
-        <div className="basis-[40%] pl-9 pt-9 text-left text-white">
+        <div className="lg:basis-[40%] md:[pl-9 pt-9] pt-5 text-left text-white">
           <p className="text-2xl font-bold mb-5">
             {detail?.title || detail?.name}
           </p>
@@ -81,8 +81,8 @@ export const Details = () => {
           </div>
         </div>
         <div className="lg:flex hidden flex-col ">
-          <p className="text-start font-semibold text-2xl text-yellow-200">{`Similar ${
-            mediaType || ""
+          <p className="text-start font-semibold text-2xl text-yellow-200">{`SIMILAR ${
+            mediaType?.toLocaleUpperCase() || ""
           }`}</p>
           <div className="grid grid-cols-2 grid-rows-2 gap-3 pt-5 mb-16 items-center">
             {similarDetails.results.map(
@@ -104,11 +104,11 @@ export const Details = () => {
 
       <section className="lg:px-36 px-10 py-10 text-justify ">
         <div className="">
-          <p>PLOT SUMMARY</p>
+          <p className="text-black font-bold underline text-xl">PLOT SUMMARY</p>
           <p>{detail?.overview}</p>
         </div>
         <div>
-          <p className="font-bold text-xl text-yellow-200">CAST</p>
+          <p className="font-bold text-xl py-5 text-yellow-200">CAST</p>
           {credit?.cast.map(
             (credit, index) =>
               index < 4 && (
@@ -121,6 +121,27 @@ export const Details = () => {
           )}
         </div>
       </section>
+
+      <div className="lg:hidden flex-col px-5">
+        <p className="font-semibold text-2xl text-yellow-200">{`SIMILAR ${
+          mediaType?.toLocaleUpperCase() || ""
+        }`}</p>
+        <div className="flex flex-row gap-10 pt-5">
+          {similarDetails.results.map(
+            (result, index) =>
+              index < 4 && (
+                <SimilarCard
+                  id={result.id}
+                  key={result.id}
+                  mediaType={mediaType!}
+                  title={result.name || result.title || ""}
+                  poster={result.poster_path}
+                  isSearchCard={false}
+                />
+              )
+          )}
+        </div>
+      </div>
     </div>
   );
 };
